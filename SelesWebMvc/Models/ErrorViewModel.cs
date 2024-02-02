1d1063bb1d7e9496aee446b3eb2f0313c8b6756f
LucasLib <lucastubecanal@gmail.com>
2024-02-02 15:18:55 -0300

    Project created

---
+++ SelesWebMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SelesWebMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
